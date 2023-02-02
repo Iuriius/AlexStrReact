bf035636873b64788369e30d092abd7643aee5c8
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-02-02 19:05:19 +0000

    Deploying to gh-pages from @ Iuriius/AlexStrReact@1d35c39964a8323b5e5f47b42707a983796c48e9 🚀

--- static/js/808.bb049fc5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_template=self.webpackChunkreact_template||[]).push([[808],{808:function(n,e,t){t.r(e),t.d(e,{default:function(){return u}});var a,i=t(168),r=t(934).Z.div(a||(a=(0,i.Z)(["\n  text-align: center;\n  @media screen and (min-width: 768px) {\n  }\n  @media screen and (min-width: 1200px) {\n  }\n"]))),c=t(184),u=function(){return(0,c.jsx)(r,{children:"Mountains"})}}}]);
-//# sourceMappingURL=808.bb049fc5.chunk.js.map
---
+++ static/js/89.6b640f81.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[89],{89:function(e,n,t){t.r(n),t.d(n,{default:function(){return d}});var r,a=t(168),i=t(934).Z.div(r||(r=(0,a.Z)(["\n  text-align: center;\n  @media screen and (min-width: 768px) {\n  }\n  @media screen and (min-width: 1200px) {\n  }\n"]))),c=t(184),d=function(){return(0,c.jsx)(i,{children:"Biography"})}}}]);
+//# sourceMappingURL=89.6b640f81.chunk.js.map
