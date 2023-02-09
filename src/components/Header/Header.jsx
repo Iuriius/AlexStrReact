ba987ed083aa217c6294e1859154a818be8700b9
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-02-09 18:41:53 +0200

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,32 +3,60 @@ import { StyledLink } from './Header.styled';
 import { Link } from 'react-router-dom';
 import Logo from '../../pictures/png/Logo-512x288.png';
 import Flag from '../../pictures/flags/ukr.png';
+import { Spin as Hamburger } from 'hamburger-react';
+import { Mobmenu } from '../Mobmenu/Mobmenu';
+
+function Open() {
+  Mobmenu.style.transform = 'translate(0vw)';
+}
+function Close() {
+  Mobmenu.style.transform = 'translate(100vw)';
+}
 
 export const Header = () => {
   return (
-    <HeaderStyled>
-      <Link to="/">
-        <img src={Logo} alt="logo" width="128px" />
-      </Link>
-      <nav className="navigation">
-        <StyledLink to="/" end>
-          Головна
-        </StyledLink>
-        <StyledLink to="/fishtogether">Зареєструватися на риболовлю</StyledLink>
-        <StyledLink to="/seminar">Замовити семінар</StyledLink>
-        <StyledLink to="/mountains">Гори гайд</StyledLink>
-        <StyledLink to="/orderbait">Замовити наживку</StyledLink>
-        <StyledLink to="/club">НХНЛ клуб</StyledLink>
-        <StyledLink to="/articles">Статті</StyledLink>
-        <StyledLink to="/biography">Біографія</StyledLink>
-      </nav>
-      <img
-        className="language"
-        src={Flag}
-        alt="logo"
-        width="20px"
-        title="Українська"
-      />
-    </HeaderStyled>
+    <>
+      <HeaderStyled>
+        <Link to="/">
+          <img src={Logo} alt="logo" width="128px" />
+        </Link>
+        <nav className="navigation">
+          <StyledLink to="/" end>
+            Головна
+          </StyledLink>
+          <StyledLink to="/fishtogether">
+            Зареєструватися на риболовлю
+          </StyledLink>
+          <StyledLink to="/seminar">Замовити семінар</StyledLink>
+          <StyledLink to="/mountains">Гори гайд</StyledLink>
+          <StyledLink to="/orderbait">Замовити наживку</StyledLink>
+          <StyledLink to="/club">НХНЛ клуб</StyledLink>
+          <StyledLink to="/articles">Статті</StyledLink>
+          <StyledLink to="/biography">Біографія</StyledLink>
+        </nav>
+        <img
+          className="language"
+          src={Flag}
+          alt="logo"
+          width="20px"
+          title="Українська"
+        />
+        <div className="burger">
+          <Hamburger
+            label="Show menu"
+            size={20}
+            color="steelblue"
+            onToggle={toggled => {
+              if (toggled) {
+                Open();
+              } else {
+                Close();
+              }
+            }}
+          />
+        </div>
+      </HeaderStyled>
+      <Mobmenu />
+    </>
   );
 };
---
+++ src/components/Mobmenu/Mobmenu.jsx
@@ -0,0 +1,18 @@
+import { StyledLink } from '../Header/Header.styled';
+
+export const Mobmenu = () => {
+  return (
+    <nav className="mobmenu">
+      <StyledLink to="/" end>
+        Головна
+      </StyledLink>
+      <StyledLink to="/fishtogether">Зареєструватися на риболовлю</StyledLink>
+      <StyledLink to="/seminar">Замовити семінар</StyledLink>
+      <StyledLink to="/mountains">Гори гайд</StyledLink>
+      <StyledLink to="/orderbait">Замовити наживку</StyledLink>
+      <StyledLink to="/club">НХНЛ клуб</StyledLink>
+      <StyledLink to="/articles">Статті</StyledLink>
+      <StyledLink to="/biography">Біографія</StyledLink>
+    </nav>
+  );
+};
