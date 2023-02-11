d1509f11f45092c1763cefb49927118efbe7b87a
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-02-11 23:56:22 +0200

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,11 @@ import { HeaderStyled } from './Header.styled';
 import { StyledLink } from './Header.styled';
 import { Link } from 'react-router-dom';
 import Logo from '../../pictures/png/Logo-512x288.png';
-import Flag from '../../pictures/flags/ukr.png';
+import Flag from '../../pictures/flags/eng.png';
 import { Spin as Hamburger } from 'hamburger-react';
 import { Mobmenu } from '../Mobmenu/Mobmenu';
+import React, { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export const Header = () => {
   function Open() {
@@ -17,49 +19,57 @@ export const Header = () => {
       'translate(100vw)';
   }
 
+  const { t, i18n } = useTranslation();
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <>
-      <HeaderStyled>
-        <Link to="/">
-          <img src={Logo} alt="logo" width="128px" />
-        </Link>
-        <nav className="navigation">
-          <StyledLink to="/" end>
-            Головна
-          </StyledLink>
-          <StyledLink to="/fishtogether">
-            Зареєструватися на риболовлю
-          </StyledLink>
-          <StyledLink to="/seminar">Замовити семінар</StyledLink>
-          <StyledLink to="/mountains">Гори гайд</StyledLink>
-          <StyledLink to="/orderbait">Замовити наживку</StyledLink>
-          <StyledLink to="/club">НХНЛ клуб</StyledLink>
-          <StyledLink to="/articles">Статті</StyledLink>
-          <StyledLink to="/biography">Біографія</StyledLink>
-        </nav>
-        <img
-          className="language"
-          src={Flag}
-          alt="logo"
-          width="20px"
-          title="Українська"
-        />
-        <div className="burger">
-          <Hamburger
-            label="Show menu"
-            size={20}
-            color="steelblue"
-            onToggle={toggled => {
-              if (toggled) {
-                Open();
-              } else {
-                Close();
-              }
-            }}
+      <Suspense fallback="loading">
+        <HeaderStyled>
+          <Link to="/">
+            <img src={Logo} alt="logo" width="128px" />
+          </Link>
+          <nav className="navigation">
+            <StyledLink to="/" end>
+              {t('Головна')}
+            </StyledLink>
+            <StyledLink to="/fishtogether">
+              Зареєструватися на риболовлю
+            </StyledLink>
+            <StyledLink to="/seminar">Замовити семінар</StyledLink>
+            <StyledLink to="/mountains">Гори гайд</StyledLink>
+            <StyledLink to="/orderbait">Замовити наживку</StyledLink>
+            <StyledLink to="/club">НХНЛ клуб</StyledLink>
+            <StyledLink to="/articles">Статті</StyledLink>
+            <StyledLink to="/biography">Біографія</StyledLink>
+          </nav>
+          <img
+            className="language"
+            src={Flag}
+            alt="logo"
+            width="20px"
+            title="English"
+            onClick={() => changeLanguage('en')}
           />
-        </div>
-      </HeaderStyled>
-      <Mobmenu />
+          <div className="burger">
+            <Hamburger
+              label="Show menu"
+              size={20}
+              color="steelblue"
+              onToggle={toggled => {
+                if (toggled) {
+                  Open();
+                } else {
+                  Close();
+                }
+              }}
+            />
+          </div>
+        </HeaderStyled>
+        <Mobmenu />
+      </Suspense>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import { App } from './App';
+import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
