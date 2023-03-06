10d6bf74c0a43804fd75b9007a52472650253646
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-03-06 22:08:10 +0200

    Update Mobmenu.jsx

--- src/components/Mobmenu/Mobmenu.jsx
+++ src/components/Mobmenu/Mobmenu.jsx
@@ -2,19 +2,31 @@ import { StyledLink } from '../Header/Header.styled';
 
 export const Mobmenu = ({ isOpen, setOpen }) => {
   return (
-    <nav className={isOpen ? 'mobmenu' : 'mobmenuActive'}>
-      <StyledLink to="/" end onClick={setOpen}>
+    <nav className={isOpen ? 'mobmenuActive' : 'mobmenu'}>
+      <StyledLink to="/" end onClick={() => setOpen(false)}>
         Головна
       </StyledLink>
-      <StyledLink to="/fishtogether" onClick={setOpen}>
+      <StyledLink to="/fishtogether" onClick={() => setOpen(false)}>
         Зареєструватися на риболовлю
       </StyledLink>
-      <StyledLink to="/seminar">Замовити семінар</StyledLink>
-      <StyledLink to="/mountains">Гори гайд</StyledLink>
-      <StyledLink to="/orderbait">Замовити наживку</StyledLink>
-      <StyledLink to="/club">НХНЛ клуб</StyledLink>
-      <StyledLink to="/articles">Статті</StyledLink>
-      <StyledLink to="/biography">Біографія</StyledLink>
+      <StyledLink to="/seminar" onClick={() => setOpen(false)}>
+        Замовити семінар
+      </StyledLink>
+      <StyledLink to="/mountains" onClick={() => setOpen(false)}>
+        Гори гайд
+      </StyledLink>
+      <StyledLink to="/orderbait" onClick={() => setOpen(false)}>
+        Замовити наживку
+      </StyledLink>
+      <StyledLink to="/club" onClick={() => setOpen(false)}>
+        НХНЛ клуб
+      </StyledLink>
+      <StyledLink to="/articles" onClick={() => setOpen(false)}>
+        Статті
+      </StyledLink>
+      <StyledLink to="/biography" onClick={() => setOpen(false)}>
+        Біографія
+      </StyledLink>
     </nav>
   );
 };
