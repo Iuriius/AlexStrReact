688ee7125dacd58f61479dea50231819ae8a9942
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-03-16 15:15:24 +0200

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,8 @@ import { HeaderStyled } from './Header.styled';
 import { StyledLink } from './Header.styled';
 import { Link } from 'react-router-dom';
 import Logo from '../../pictures/png/Logo-512x288.png';
-import Flag from '../../pictures/flags/eng.png';
+import usFlag from '../../pictures/flags/eng.png';
+// import ukFlag from '../../pictures/flags/ukr.png';
 import { Spin as Hamburger } from 'hamburger-react';
 import { Mobmenu } from '../Mobmenu/Mobmenu';
 import React, { Suspense, useState } from 'react';
@@ -28,18 +29,18 @@ export const Header = () => {
               {t('Головна')}
             </StyledLink>
             <StyledLink to="/fishtogether">
-              Зареєструватися на риболовлю
+              {t('Зареєструватися на риболовлю')}
             </StyledLink>
-            <StyledLink to="/seminar">Замовити семінар</StyledLink>
-            <StyledLink to="/mountains">Гори гайд</StyledLink>
-            <StyledLink to="/orderbait">Замовити наживку</StyledLink>
-            <StyledLink to="/club">НХНЛ клуб</StyledLink>
-            <StyledLink to="/articles">Статті</StyledLink>
-            <StyledLink to="/biography">Біографія</StyledLink>
+            <StyledLink to="/seminar">{t('Замовити семінар')}</StyledLink>
+            <StyledLink to="/mountains">{t('Гори гайд')}</StyledLink>
+            <StyledLink to="/orderbait">{t('Замовити наживку')}</StyledLink>
+            <StyledLink to="/club">{t('НХНЛ клуб')}</StyledLink>
+            <StyledLink to="/articles">{t('Статті')}</StyledLink>
+            <StyledLink to="/biography">{t('Біографія')}</StyledLink>
           </nav>
           <img
             className="language"
-            src={Flag}
+            src={usFlag}
             alt="logo"
             width="20px"
             title="English"
