f2d5657c6690b34ad3f1487f07f98c3677fa2e04
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-03-19 13:40:33 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,24 +1,17 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
-import common_uk from './locales/uk/translation.json';
-import common_en from './locales/en/translation.json';
+import { initReactI18next } from 'react-i18next';
+import uk from './locales/uk/translation.json';
+import en from './locales/en/translation.json';
 
-i18n
-  .use(Backend)
-  .use(initReactI18next)
-  .init({
-    lng: 'uk',
-    fallbackLng: 'en',
-    debug: true,
-    resources: {
-      en: {
-        common: common_en,
-      },
-      uk: {
-        common: common_uk,
-      },
-    },
-  });
+i18n.use(Backend).use(initReactI18next).init({
+  lng: 'uk',
+  fallbackLng: 'en',
+  debug: true,
+  resources: {
+    en,
+    uk,
+  },
+});
 
 export default i18n;
